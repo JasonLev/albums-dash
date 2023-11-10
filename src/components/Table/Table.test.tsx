1c9cfd0bf77dc79c8a8947b5b95527d8933696aa
Jason Levy <jasond.levy@gmail.com>
2023-11-10 13:11:46 -0800

    Updates

--- src/components/Table/Table.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Table from "./Table";
-
-test("renders the Table's heading text: 'Albums Details'", () => {
-  render(<Table />);
-  const tableHeadingText = screen.getByText(/Albums Details/i);
-  expect(tableHeadingText).toBeInTheDocument();
-});
